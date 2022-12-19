40f3d5a620f651fc9b8b50338a9f44265e710704
Denis <110766239+3dmatern@users.noreply.github.com>
2022-12-20 02:34:14 +0300

    Update rePassword.php

--- vendor/rePassword.php
+++ vendor/rePassword.php
@@ -1,9 +1,8 @@
 <?php
     session_start();
     //Подключаем класс
-    use classes\User;
-    function my_autoload($className){
-        require_once('../'.$className.'.class.php');
+    function my_autoload($classname){
+        require_once("..\\classes\\{$classname}.class.php");
     }
     spl_autoload_register('my_autoload');
 
