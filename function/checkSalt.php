5e7332bb6250537e7e6c0d24e9cd7d961f7dabf3
Denis <110766239+3dmatern@users.noreply.github.com>
2022-12-20 02:32:58 +0300

    Update checkSalt.php

--- function/checkSalt.php
+++ function/checkSalt.php
@@ -3,14 +3,12 @@
     {
         $json = file_get_contents('../vendor/db.json');
         $jsonArray = json_decode($json, true);
-        $i = -1;
-        $count=count($jsonArray);
-        while($i<$count){
-            $i++;
-            $result = $jsonArray[$i][0];
-            if($result === $login){
-                $salt = $jsonArray[$i][2];
-                return $salt;
+        foreach($jsonArray as $key => $value){
+            for($i=0, $count=count($jsonArray); $i<$count; $i++){
+                if($value[$i] === $login){
+                    $salt = $value[2];
+                    return $salt;
+                }
             }
         }
     }
